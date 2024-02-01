108b92552cd39a385cb67540704dc576e6771406
coderbabu00 <coderbabu00@gmail.com>
2024-01-31 19:16:55 -0800

    first commit

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        minLength:5
+},
+email:{
+        type:String,
+        required:true,
+        unique:true  
+},
+password: {
+type: String,
+required: true,
+minlength: 6
+},
+
+Followers:{
+        type:Array,
+},
+Following:{
+        type:Array,
+},
+phonenumber:{
+        type:Number,
+        required:true
+},
+profile:{
+        type:String,
+},
+verifed:{
+        type:Boolean,
+        required:true,
+        default:false
+}
+})
+
+module.exports = mongoose.model("User", userSchema)
